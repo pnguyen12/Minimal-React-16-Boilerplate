393a5f11f928fbd6a6f21184a532f034d6de0bba
pnguyen12 <peterndevelopment@gmail.com>
2018-12-08 23:38:57 -0700

    (feat) - atoms layout and Button Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
+import Button from './atoms/Button'; 
 
 const Home = ({ title }) => {
     return (
         <div className="home-wrapper">
             <header className="home-header">
                 <p className="home-header-text">{title}</p>
+                <Button text="Get Started" className="btn btn-main" />
             </header>
         </div>
     )
---
+++ src/components/atoms/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Button = ({ text, className }) => <a href="#" className={String(className)}>{text}</a>
+
+export default Button;

16ef6e2786b93db8b5b16330ece9b8da75e8a777
pnguyen12 <peterndevelopment@gmail.com>
2019-03-18 23:07:01 -0700

    chore(config): ignore files for eslint and prettier

--- __tests__/components/button.js
+++ __tests__/components/button.js
@@ -6,21 +6,15 @@ import Button from '../../src/components/atoms/Button';
 configure({ adapter: new Adapter() });
 
 const setup = () => {
-    const state = {};
-    const props = { className: 'button-class', text: 'A sophisticated button' }
-    const enzymeWrapper = shallow(<Button {...state} {...props} />);
-    return { enzymeWrapper };
-}
+  const state = {};
+  const props = { className: 'button-class', text: 'A sophisticated button' };
+  const enzymeWrapper = shallow(<Button {...state} {...props} />);
+  return { enzymeWrapper };
+};
 
 const { enzymeWrapper } = setup();
 
 it('Should have a root div with className as App', () => {
-    expect(enzymeWrapper.find('a').hasClass('button-class')).toBe(true);
-    expect(enzymeWrapper.text().includes('A sophisticated button')).toBe(true)
-})
-
-
-
-
-
-
+  expect(enzymeWrapper.find('a').hasClass('button-class')).toBe(true);
+  expect(enzymeWrapper.text().includes('A sophisticated button')).toBe(true);
+});

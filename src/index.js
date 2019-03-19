fc8a99340ca11e82ed4533c46f1e27c681a64f3b
pnguyen12 <peterndevelopment@gmail.com>
2019-03-18 22:53:39 -0700

    chore(config): .vscode settings config

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import Home from './components/Home';
 import './static/styles/base.scss';
 
-const title = 'React Boilerplate'
+const title = 'React Boilerplate';
 const sub = 'Start building';
 
-const App = () => {
-    return (
-        <div className="app">
-            <Home title={title} sub={sub}/>
-        </div>
-    )
-}
+const App = () => (
+  <div className="app">
+    <Home title={title} sub={sub} />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
-

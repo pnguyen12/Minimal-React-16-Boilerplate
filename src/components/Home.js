659afb2837c58d9bf7ca36574096dd14834a1cec
pnguyen12 <peterndevelopment@gmail.com>
2019-03-18 23:04:01 -0700

    chore(scripts): add scripts for linting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from './atoms/Button'; 
+import Button from './atoms/Button';
 
-const Home = ({ title, sub }) => {
-    return (
-        <div className="home-wrapper">
-            <header className="home-header">
-                <p className="home-header-text">{title}</p>
-                <p className="home-header-sub">{sub}</p>
-                <Button text="Get Started" className="btn btn-white btn-animate" />
-            </header>
-        </div>
-    )
-}
+const Home = ({ title, sub }) => (
+  <div className="home-wrapper">
+    <header className="home-header">
+      <p className="home-header-text">{title}</p>
+      <p className="home-header-sub">{sub}</p>
+      <Button text="Get Started" className="btn btn-white btn-animate" />
+    </header>
+  </div>
+);
 
 Home.propTypes = {
-    title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+};
 
 export default Home;
--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Button = ({ text, className }) => <a href="#" className={String(className)}>{text}</a>
+const Button = ({ text, className }) => (
+  <a href="#" className={String(className)}>
+    {text}
+  </a>
+);
 
 export default Button;
